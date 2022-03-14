523dbe85eca8ff000cab46f85e5dbd832b6a2fc2
Pavan Chitradurga Thammanna <mail@pavanct.com>
2022-03-14 04:30:59 +0100

    add basic Contacts schema

---
+++ src/graphql/schema.js
@@ -0,0 +1,59 @@
+import { gql } from "apollo-server"
+
+export const typeDefs = gql`
+  type Phone {
+    work: String
+    home: String
+    mobile: String
+    other: String
+  }
+
+  type Address {
+    city: String
+    street: String
+    postalCode: String
+    country: String
+  }
+
+  type Contact {
+    id: ID!
+    firstName: String!
+    lastName: String
+    email: String
+    phone: Phone
+    address: Address
+    createdAt: String
+    updatedAt: String
+  }
+
+  input ContactInput {
+    firstName: String!
+    lastName: String
+    email: String
+    phone: PhoneInput
+    address: AddressInput
+  }
+
+  input PhoneInput {
+    work: String
+    home: String
+    mobile: String
+    other: String
+  }
+
+  input AddressInput {
+    city: String
+    street: String
+    country: String
+    postalCode: String
+  }
+
+  type Query {
+    contacts(): [Contact]
+    contact(id: ID!): Contact
+  }
+
+  type Mutation {
+    createContact(Contact: ContactInput!): Contact!
+  }
+`
